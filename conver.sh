02bd2e8722d53baa5f741dbebd993ded5c3d6b51
john.b.y.yang <john.b.y.yang@gmail.com>
2018-05-16 18:19:55 +0800

    add some convert note

---
+++ conver.sh
@@ -0,0 +1,2 @@
+ffmpeg -i '/mnt/data/temp/Game.of.Thrones.S01.720p.BluRay.x264.ShAaNiG/Game.of.Thrones.S01E02.720p.BluRay.450MB.ShAaNiG.com.mkv' -codec copy /home/john/nfs/GameOfT/S01E02.mp4
+ffmpeg -i '/mnt/data/temp/Game.of.Thrones.S01.720p.BluRay.x264.ShAaNiG/sub/1/Game.of.Thrones.S01E02.The.Kingsroad.1080p.BluRay.10Bit.x265.Opus-CommIE.ass' 2.vtt

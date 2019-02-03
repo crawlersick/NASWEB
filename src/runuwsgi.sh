1d175b6ab0c9c1aa1eee1cd4633957792e0a9431
john.b.y.yang@gmail.com <john.b.y.yang@gmail.com>
2019-02-04 00:30:52 +0800

    ,,,

--- src/runuwsgi.sh
+++ src/runuwsgi.sh
@@ -1,2 +1,7 @@
 #uwsgi -s /tmp/uwsgi.sock --plugin python --module hello --callable app
-uwsgi --http 0.0.0.0:5001 --plugin python --module hello --callable app --stats 127.0.0.1:9191 --enable-threads -d /tmp/uwsgipid --logto2 /tmp/uwsgilog.log
+
+uwsgi --http 0.0.0.0:5000 --plugin python --module hello --callable app --stats 127.0.0.1:9191 --enable-threads -d /tmp/uwsg.log --pidfile /tmp/uwsgipid
+#stop with uwsgi command:
+#uwsgi --stop /tmp/uwsgipid
+#stop
+#ps -ef|grep uwsgi|grep hello|awk '{print $2}'|xargs kill -9

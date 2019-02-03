8b5fc47bd68979ec15927f1a1653b88e26379b01
john.b.y.yang@gmail.com <john.b.y.yang@gmail.com>
2019-02-04 00:16:21 +0800

    add uwsgi

--- src/runuwsgi.sh
+++ src/runuwsgi.sh
@@ -1,1 +1,2 @@
-uwsgi -s /tmp/uwsgi.sock --plugin python --module hello --callable app
+#uwsgi -s /tmp/uwsgi.sock --plugin python --module hello --callable app
+uwsgi --http 0.0.0.0:5001 --plugin python --module hello --callable app --stats 127.0.0.1:9191 --enable-threads -d /tmp/uwsgipid --logto2 /tmp/uwsgilog.log

3577c3e50532529fe747b6487700a38f530b4e9a
john.b.y.yang@gmail.com <john.b.y.yang@gmail.com>
2019-02-04 00:32:43 +0800

    mmmm

--- auto_job.sh
+++ auto_job.sh
@@ -6,5 +6,6 @@ pwd > /tmp/tttttt
 nohup ./auto_down_bat.sh > /home/sick/Downloads/auto.log 2>&1 &
 #nohup ./ep_autodown.sh > /tmp/ep.log 2>&1 &
 cd /home/sick/NASWEB/src
-nohup ./run.sh > /tmp/NASWEB.log 2>&1 &
+#nohup ./run.sh > /tmp/NASWEB.log 2>&1 &
+./runuwsgi.sh
 exit 0

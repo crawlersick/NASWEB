ccd32a8b339c9022d8ed04aabd98484893a9109b
pi <pi@example.com>
2019-10-06 14:21:58 +0800

    add processes

--- src/runuwsgi.sh
+++ src/runuwsgi.sh
@@ -1,6 +1,6 @@
 #uwsgi -s /tmp/uwsgi.sock --plugin python --module hello --callable app
 #uwsgi --http 0.0.0.0:5000 --plugin python --module hello --callable app --stats 127.0.0.1:9191 --enable-threads -d /tmp/uwsg.log --pidfile /tmp/uwsgipid
-uwsgi_python3 --http-socket 0.0.0.0:5000  --module hello --callable app --stats 127.0.0.1:9191 --enable-threads -d /tmp/uwsg.log --pidfile /tmp/uwsgipid
+uwsgi_python3 --http-socket 0.0.0.0:5000  --module hello --callable app --stats 127.0.0.1:9191 --master --processes 10 --threads 5 -d /tmp/uwsg.log --pidfile /tmp/uwsgipid
 #stop with uwsgi command:
 #uwsgi --stop /tmp/uwsgipid
 #stop

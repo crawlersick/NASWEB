3b76e585bc3ad704164633789a388f23426ed9ee
Your Name <you@example.com>
2019-10-06 22:07:45 +0800

    ,,

--- src/runuwsgi.sh
+++ src/runuwsgi.sh
@@ -1,7 +1,9 @@
-#uwsgi -s /tmp/uwsgi.sock --plugin python --module hello --callable app
-#uwsgi --http 0.0.0.0:5000 --plugin python --module hello --callable app --stats 127.0.0.1:9191 --enable-threads -d /tmp/uwsg.log --pidfile /tmp/uwsgipid
-uwsgi_python3 --http-socket 0.0.0.0:5000  --module hello --callable app --stats 127.0.0.1:9191 --master --processes 10 --threads 5 -d /tmp/uwsg.log --pidfile /tmp/uwsgipid
-#stop with uwsgi command:
-#uwsgi --stop /tmp/uwsgipid
-#stop
+if [[ -z $1  ]]
+then
+
+uwsgi --http 0.0.0.0:5000 --plugin python  --module hello --callable app --stats 127.0.0.1:9191 --master --processes 10 --threads 5 -d /tmp/uwsg.log --pidfile /tmp/uwsgipid
+
+else
+uwsgi --stop /tmp/uwsgipid
 #ps -ef|grep uwsgi|grep hello|awk '{print $2}'|xargs kill -9
+fi

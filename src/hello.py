866164f33b90df7030de392430b8473e04b4fd05
john <johnbyyang@gmail.com>
2020-03-01 00:42:23 +0800

    m

--- src/hello.py
+++ src/hello.py
@@ -3,11 +3,26 @@ from flask import jsonify
 import myutils.getDindex
 import os
 import urllib
+from flask_httpauth import HTTPBasicAuth
+from werkzeug.security import generate_password_hash, check_password_hash
 
 app = Flask(__name__)
 app.config['JSON_AS_ASCII'] = False
 
+auth = HTTPBasicAuth()
+
+users = {
+    "john": generate_password_hash("123")
+}
+
+@auth.verify_password
+def verify_password(username, password):
+    if username in users:
+        return check_password_hash(users.get(username), password)
+    return False
+
 @app.route('/')
+@auth.login_required
 def hello_world():
     #return 'Hello, World!'
     return render_template('home.html')
@@ -27,7 +42,7 @@ def l2list(epname):
 @app.route('/l3/<foldername>/<epname>')
 def l3list(foldername,epname):
     epname=epname.replace('-->',os.sep)
-    
+
     filename=os.path.join("/static/Downloads/",foldername,epname)
     filename=urllib.parse.quote(filename)
     return render_template('play.html',filename=filename,subname=filename+".vtt")
